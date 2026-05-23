3824ad1fadf8c615696fd22543199a43bf4cb739
Nguyen Loc <locdedoda@gmail.com>
2026-05-23 23:05:40 +0700

    package: add vue devtools

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import App from './App.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
+if (import.meta.env.DEV) {
+  const script = document.createElement('script');
+  script.src = 'http://localhost:8098';
+  document.head.appendChild(script);
+}
+
 const app = createApp(App);
 app.use(ElementPlus);
 app.mount('#app').$nextTick(() => {
